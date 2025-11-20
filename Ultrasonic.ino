2010fd5e6f6319cfe105c3a7acd9d80c9028d6e6
soniakshat479-cmd <soniakshat479@gmail.com>
2025-11-20 15:31:00 +0530

    ultrasonic

---
+++ Ultrasonic.ino
@@ -0,0 +1,26 @@
+#define trigPin 2
+#define echoPin 3
+
+long duration;
+int distance;
+
+void setup() 
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(50);
+}
